992679f2b7ed66698c5b3c208f86942201b2ef4b
code1218 <skjil1218@gmail.com>
2024-03-06 17:17:04 +0900

    ioc

--- src/main/java/com/study/mvc/service/CarServiceImpl.java
+++ src/main/java/com/study/mvc/service/CarServiceImpl.java
@@ -2,17 +2,10 @@ package com.study.mvc.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.mvc.repository.CarRepository;
-import com.study.mvc.repository.CarRepositoryImpl;
-import com.study.mvc.repository.CarRepositoryImpl2;
-import com.study.mvc.util.TestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService {
